4cf22444e08d7ebf74f6be2c435c3b38fec7a650
ure8562 <sjwnrdjtj@naver.com>
2023-01-06 13:40:20 +0900

    usb를 이용한 serial 통신 코드추가 코틀린 언어로 짜였으며, 쓰레드를 통해 수신인터럽트 감지한다. hex to byte 와 byte to hex 동작확인 다음스텝으로는 시리얼통신용 앱 제작 테스트

--- app/src/main/java/com/example/echo_m/MainActivity.kt
+++ app/src/main/java/com/example/echo_m/MainActivity.kt
@@ -1,82 +1,141 @@
 package com.example.echo_m
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.usb.UsbManager
 import android.os.Bundle
-import android.util.Log
-import android_serialport_api.SerialPort
-import android_serialport_api.SerialPortFinder
-import java.io.File
+import androidx.appcompat.app.AppCompatActivity
+import com.example.echo_m.databinding.ActivityMainBinding
+import com.hoho.android.usbserial.driver.UsbSerialPort
+import com.hoho.android.usbserial.driver.UsbSerialProber
 import java.io.IOException
-import java.io.InputStream
-import java.io.OutputStream
+import java.lang.Thread.interrupted
+import java.math.BigInteger
+
 
 class MainActivity : AppCompatActivity() {
 
-    val SERIAL_PORT_NAME = "ttyS0"
-    val SERIAL_BAUDRATE = 115200
-
-    var serialPort: SerialPort? = null
-    var inputStream: InputStream? = null
-    var outputStream: OutputStream? = null
     //test1
+    var port:UsbSerialPort ?= null
     lateinit var serialThread: Thread
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
-        OpenSerialPort(SERIAL_PORT_NAME)
-        StartRxThread()
-    }
+        // 바인딩 초기화
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        // 레이아웃(root뷰) 표시
+        setContentView(binding.root)
 
-    private fun OpenSerialPort(name: String){
-        val serialPortFinder: SerialPortFinder = SerialPortFinder();
-        val devices: Array<String> = serialPortFinder.allDevices
-        val devicesPath: Array<String> = serialPortFinder.allDevicesPath
+        //setContentView(R.layout.activity_main);
+        registerReceiver(mUsbDeviceReceiver, IntentFilter(UsbManager.ACTION_USB_DEVICE_ATTACHED))
 
-        for(device in devices){
-            if(device.contains(name, true)){
-                val index = devices.indexOf(device)
-                serialPort = SerialPort(File(devicesPath.get(index)), SERIAL_BAUDRATE, 0)
-                break;
+        binding.button2.setOnClickListener{
+
+            var temp:String = "AE01"
+            var temp2:ByteArray = temp.decodeHex()
+            port?.write(temp2,100)
+        }
+
+        binding.button.setOnClickListener{
+
+            val manager = getSystemService(USB_SERVICE) as UsbManager
+            val availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager)
+            if (availableDrivers.isEmpty()) {
+                return@setOnClickListener;
+            }
+            val driver = availableDrivers[0]
+            val connection = manager.openDevice(driver.device)
+            val hasPermision = manager.hasPermission(driver.device)
+            if (connection == null) {
+                //UsbManager.requestPermission(driver.getDevice(), mPendingIntent)
+                // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
+                return@setOnClickListener
+            }
+            port = driver.ports[0] // Most devices have just one port (port 0)
+
+            try {
+                port?.open(connection)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            try {
+                port?.setParameters(921600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            serialThread.start()
+        }
+
+        serialThread = Thread() {
+            var mKillSign = false
+
+            // stop this thread
+            fun setKillSign(isTrue: Boolean) {
+                mKillSign = true
+            }
+            while(mKillSign == false)
+            {
+                val readBuffer = ByteArray(4096)
+                while (!interrupted()) {
+                    try {
+                        // Read and Display to Terminal
+                        val numBytesRead: Int = port?.read(readBuffer, 2000) ?: -1
+
+
+                        if (numBytesRead > 0) {
+                            val Hex_Value: String =
+                                getByte_To_HexString(readBuffer, numBytesRead)
+                            val Hex_Value1: String =
+                                getByte_To_HexString(readBuffer, numBytesRead)
+                        }
+                    } catch (e: IOException) {
+                    }
+                    try {
+                        Thread.sleep(100)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                        break
+                    }
+                    if (mKillSign) break
+                }
             }
         }
 
-        serialPort?.let{
-            inputStream = it.inputStream
-            outputStream = it.outputStream
+
+    }
+
+
+
+    private val mUsbDeviceReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (UsbManager.ACTION_USB_DEVICE_ATTACHED == action) {
+            } else if (UsbManager.ACTION_USB_DEVICE_DETACHED == action) {
+            }
         }
     }
 
-    private fun StartRxThread(){
-        if(inputStream == null) {
-            Log.e("SerialExam", "Can't open inputstream")
-            return
+    fun getByte_To_HexString(buf: ByteArray, size: Int): String {
+        var Hex_Value = ""
+        for (i in 0 until size) {
+            Hex_Value += String.format("0x%02x ", buf[i]) //x가 소문자면 소문자 출력, 대문자면 대문자 출력
         }
-        serialThread = Thread{
-            while(true)
-                try{
-                    val buffer = ByteArray(64)
-                    val size = inputStream?.read(buffer)
-                    onReceiveData(buffer, size?:0)
-                }catch (e: IOException) {e.printStackTrace()}
-        }
-        serialThread.start()
+        return Hex_Value
     }
 
-    private fun onReceiveData(buffer: ByteArray, size: Int){
-        if(size < 1) return
+    fun String.decodeHex(): ByteArray {
+        check(length % 2 == 0) { "Must have an even length" }
 
-        var strBuilder = StringBuilder()
-        for(i in 0 until size){
-            strBuilder.append(String.format("%02x ", buffer[i]))
-        }
-        Log.d("SerialExam", "rx: " + strBuilder.toString())
+        val byteIterator = chunkedSequence(2)
+            .map { it.toInt(16).toByte() }
+            .iterator()
+
+        return ByteArray(length / 2) { byteIterator.next() }
     }
 
-    private fun SendData(data: ByteArray){
-        try{
-            outputStream?.write(data)
-        }catch (e:IOException) {e.printStackTrace()}
-    }
+
 }
